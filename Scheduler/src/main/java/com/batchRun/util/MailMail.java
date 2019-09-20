c1aff3f1f02a65079bb5771acabc93eb32214489
Rakpati <49346005+Rakpati@users.noreply.github.com>
2019-09-20 13:32:49 +0530

    Update MailMail.java

--- Scheduler/src/main/java/com/batchRun/util/MailMail.java
+++ Scheduler/src/main/java/com/batchRun/util/MailMail.java
@@ -1,18 +1,26 @@
 package com.batchRun.util;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeSet;
+
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 
+import org.apache.velocity.app.VelocityEngine;
 import org.springframework.mail.MailParseException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
+import org.springframework.ui.velocity.VelocityEngineUtils;
 
 public class MailMail
 {
 	private JavaMailSender mailSender;
 	private SimpleMailMessage simpleMailMessage;
-	
+	private VelocityEngine velocityEngine;
+
 	public void setSimpleMailMessage(SimpleMailMessage simpleMailMessage) {
 		this.simpleMailMessage = simpleMailMessage;
 	}
@@ -20,28 +28,34 @@ public class MailMail
 	public void setMailSender(JavaMailSender mailSender) {
 		this.mailSender = mailSender;
 	}
-	
-	public void sendMail(String dear, String emailId, String content) {
-	
-		MimeMessage message = mailSender.createMimeMessage();
-		
-		try{
-			MimeMessageHelper helper = new MimeMessageHelper(message, true);
-			
-			helper.setFrom(simpleMailMessage.getFrom());
-			helper.setTo(emailId);
-			helper.setSubject(simpleMailMessage.getSubject());
-			helper.setText(String.format(
-					simpleMailMessage.getText(), dear, content));
-			System.out.println("1");
 
-		}catch (MessagingException e) {
-			throw new MailParseException(e);
-		}
-		mailSender.send(message);
-		System.out.println("Done");
-		
+	public void setVelocityEngine(VelocityEngine velocityEngine) {
+		this.velocityEngine = velocityEngine;
 	}
-	
-	
+
+	public void sendMail(final TreeSet<String> name, final TreeSet<String> email, final TreeSet<Integer> days) {
+		
+		MimeMessagePreparator preparator = new MimeMessagePreparator() {
+            public void prepare(MimeMessage mimeMessage) throws Exception {
+            	String text ="";
+                MimeMessageHelper message = new MimeMessageHelper(mimeMessage);
+
+                message.setTo(email.last());
+                message.setFrom(simpleMailMessage.getFrom()); 
+                message.setSubject(simpleMailMessage.getSubject());
+                Map<String, Object> model = new HashMap();
+                model.put("user", name.last());
+                if(days.last() == 20) {
+                	text = VelocityEngineUtils.mergeTemplateIntoString(
+                			velocityEngine, "finalReminderTemplate.vm", model);
+                }else {
+                	text = VelocityEngineUtils.mergeTemplateIntoString(
+                			velocityEngine, "reminderTemplate.vm", model);
+                }
+                message.setText(text, true);
+            }
+        };
+        this.mailSender.send(preparator);
+        System.out.println("Mail Sent "+name.last());
+	}
 }
